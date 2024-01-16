2ac46e5decc030d85e7845a0a8cfc13be3e25800
Akshat Mathur <star5akshat@gmail.com>
2024-01-17 00:10:00 +0530

    some minor fixes

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import "./Home.scss";
 import Chip from './Chip';
 import Modal from './Modal';
@@ -14,15 +14,19 @@ const Home = () => {
     const [modalData, setModalData] = useState<DataType[]>(data);
     const [chipData, setChipData] = useState<DataType[]>([]);
     const [lastUpdatedModalData, setLastUpdatedModalData] = useState<DataType[]>(data);
+    const inputRef = useRef<any>(null);
     const handleCrossClick = (item: any) => {
         setChipData(chipData?.filter((ele) => ele?.id !== item?.id));
         setModalData([...modalData, item]);
         setLastUpdatedModalData([...lastUpdatedModalData, item]);
     };
     const handleModalItemClick = (item:any) => {
-        setModalData(modalData.filter((ele) => ele?.id !== item?.id));
+        setModalData(lastUpdatedModalData.filter((ele) => ele?.id !== item?.id));
         setChipData([...chipData, item]);
         setLastUpdatedModalData(lastUpdatedModalData?.filter((ele) => ele?.id !== item?.id));
+        if(inputRef.current){
+            inputRef.current.value="";
+        }
     };
     const handleChange = (e: any) => {
         if(e.target.value.length === 0){
@@ -31,12 +35,17 @@ const Home = () => {
             const filteredData = lastUpdatedModalData?.filter((item) => item?.name?.toLowerCase()?.includes(e.target.value?.toLowerCase()));
             setModalData(filteredData);
         }
+    };
+    const handlBarClick = () => {
+        if(inputRef.current){
+            inputRef?.current?.focus();
+        }
     }
     return <div className='root'>
         <div className='heading'>Pick Users</div>
-        <div className='container'>
+        <div className='container' onClick={handlBarClick}>
             <Chip chipData={chipData} handleCrossClick={handleCrossClick}/>
-            <div className='input'><input type="text" onChange={(e) => handleChange(e)} placeholder={`${chipData.length === 0? 'Type User form below': ''}`}/></div>
+            <div className='input'><input ref={inputRef} type="text" onChange={(e) => handleChange(e)} placeholder={`${chipData.length === 0? 'Type User from below': ''}`}/></div>
         </div>
      <Modal modalData={modalData} handleModalItemClick={handleModalItemClick}/>
     </div>
